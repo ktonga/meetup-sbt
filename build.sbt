6823526b90d5cf57c6d9f274116b68d0044e9ce3
Gaston M. Tonietti <gtonietti@despegar.com>
2014-03-17 14:59:50 -0300

    Refactoring to .scala

--- build.sbt
+++ build.sbt
@@ -1,16 +1,13 @@
+import Time._
+import Dependencies._
 
 name := "meetup-sbt"
 
 version := "1.0"
 
-libraryDependencies ++= Seq(
-  "org.quartz-scheduler"  %   "quartz"            % "1.8.0",
-  "ch.qos.logback"        %   "logback-classic"   % "1.0.13",
-  "org.scalaz"            %%  "scalaz-core"       % "7.0.4",
-  "joda-time"             %   "joda-time"         % "2.1",
-  "org.scalatest"         %%  "scalatest"         % "2.1.0"   % "test",
-  "org.scalacheck"        %%  "scalacheck"        % "1.11.0"  % "test"
-)
+timeSettings
+
+libraryDependencies ++= dependencies
 
 net.virtualvoid.sbt.graph.Plugin.graphSettings
 
---
+++ project/Time.scala
@@ -0,0 +1,26 @@
+import java.text.DateFormat
+import java.util.Date
+import sbt._
+
+object Time {
+
+  lazy val staticTime = settingKey[Date]("Build launch time")
+  lazy val formatStyle = settingKey[Int]("Style for formatting date and time")
+  lazy val formatter = settingKey[DateFormat]("Date Formatter")
+  lazy val dynamicTime = taskKey[Date]("Generates a Date for the current time")
+  lazy val formatTime = taskKey[String]("Formats the current time")
+  lazy val printTime = taskKey[Unit]("Prints the current time")
+
+  val timeSettings = Seq(
+    staticTime := new Date,
+    dynamicTime := new Date,
+    formatStyle := DateFormat.SHORT,
+    formatter := {
+      val style = formatStyle.value
+      DateFormat.getDateTimeInstance(style, style)
+    },
+    formatTime := formatter.value.format(dynamicTime.value),
+    printTime := println("Time: " + formatTime.value)
+  )
+
+}
